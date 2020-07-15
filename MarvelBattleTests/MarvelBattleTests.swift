e0e112c652cbfd7ccbd17f2adf1780f37fc0aa3a
José Luis Fernández Bueno <joseluis.fernandez@intelygenz.com>
2020-07-15 17:51:45 +0200

    Initial Commit

---
+++ MarvelBattleTests/MarvelBattleTests.swift
@@ -0,0 +1,34 @@
+//
+//  MarvelBattleTests.swift
+//  MarvelBattleTests
+//
+//  Created by Jose Luis Fernandez on 15/07/2020.
+//  Copyright © 2020 joseluisfernandezbueno. All rights reserved.
+//
+
+import XCTest
+@testable import MarvelBattle
+
+class MarvelBattleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
